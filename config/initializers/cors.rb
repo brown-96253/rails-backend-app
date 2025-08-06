b45442ae70399bb40b2e888474604352502851c3
Brown <brown-96253@users.noreply.github.com>
2025-08-06 16:48:50 +0900

    chore: 本番環境のCORS設定にRailwayのURLを追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000", "https://gourmet-app-rails-test-production.up.railway.app/"
     resource "*",
              headers: :any,
-             methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
              credentials: false
   end
 end
