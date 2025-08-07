a9f8e382a2cdd61b7478f280aa97724bfe185a4c
Brown <brown-96253@users.noreply.github.com>
2025-08-07 16:46:29 +0900

    debug: ヘルスチェック用ルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # デバッグ用ルート
+  get '/health', to: proc { [200, {'Content-Type' => 'text/plain'}, ['Rails OK']] }
+  get '/test-api', to: proc { [200, {'Content-Type' => 'application/json'}, ['{"message": "API works"}']] }
+  
+  # APIルート
   namespace :api do
-    resources :posts, only: [ :index, :create ]
+    resources :posts, only: [:index, :create]
   end
 end
