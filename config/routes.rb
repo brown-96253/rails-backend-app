f7e16b8ac7134d9b44f98667fe701acb78252e67
Brown <brown-96253@users.noreply.github.com>
2025-08-08 22:28:55 +0900

    style: Rubocop指摘のレイアウト違反を修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # デバッグ用ルート
   get "/health", to: proc { [ 200, { "Content-Type" => "text/plain" }, [ "Rails OK" ] ] }
   get "/test-api", to: proc { [ 200, { "Content-Type" => "application/json" }, [ '{"message": "API works"}' ] ] }
-  
+
   # APIルート
   namespace :api do
     resources :posts, only: [ :index, :create, :show ]
